b40067773f0e580b24bf41230b5e76e148960d0e
Austin Taylor Mann <Fancysause@gmail.com>
2020-09-15 19:28:42 -0400

    added warning and success components

--- assignment-one/src/app/app.module.ts
+++ assignment-one/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningComponentComponent } from './warning-component/warning-component.component';
+import { SuccessComponentComponent } from './success-component/success-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponentComponent,
+    SuccessComponentComponent
   ],
   imports: [
     BrowserModule
