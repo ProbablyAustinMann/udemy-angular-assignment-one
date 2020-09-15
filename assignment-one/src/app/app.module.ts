22b3f7d68821e61d7c14461a935c69a7b7192b4a
Austin Taylor Mann <Fancysause@gmail.com>
2020-09-15 19:41:56 -0400

    displayed components in the app component with appropriate styling

--- assignment-one/src/app/app.module.ts
+++ assignment-one/src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WarningComponentComponent } from './warning-component/warning-component.component';
-import { SuccessComponentComponent } from './success-component/success-component.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningComponentComponent,
-    SuccessComponentComponent,
     SuccessAlertComponent,
     WarningAlertComponent
   ],
